ec85b2041e6f88e6dfaeda2e2c091c373a925ead
AlexJKL <alexandr_sidorenko@icloud.com>
2025-01-24 22:53:53 +0200

    1.6 Done | Unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,60 +1,22 @@
 /* eslint-disable react/prop-types */
-const Header = (props) => {
-  return (
-    <div>
-    <h1>{props.course.name}</h1>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.course.parts.map((part, index) => (
-        <p key={index}>
-          {part.name} {part.exercises}
-        </p>
-      ))}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  let totalExercises = 0
-  for (const part of props.course.parts) {
-    totalExercises += part.exercises;
-  }
-  return (
-    <div>
-      <p>Number of exercises {totalExercises}</p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <h1>Statistic</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
