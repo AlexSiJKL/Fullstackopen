b4dffcd7112c09676d1ccad3242c9a617b773eab
AlexJKL <alexandr_sidorenko@icloud.com>
2025-01-25 14:03:09 +0200

    1.8 Done | Unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,20 @@
 /* eslint-disable react/prop-types */
 import { useState } from 'react'
 
+const Statistic = ({good, neutral, bad}) => {
+  return (
+    <div>
+    <h1>Statistic</h1>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {good + neutral + bad}</p>
+    <p>average {(good - bad) / (good + neutral + bad)}</p>
+    <p>positive {good / ((good + neutral + bad) / 100)} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -13,13 +27,10 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>Good</button>
       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
       <button onClick={() => setBad(bad + 1)}>Bad</button>
-      <h1>Statistic</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {good / ((good + neutral + bad) / 100)} %</p>
+      <Statistic 
+      good={good}
+      neutral={neutral}
+      bad={bad} />
     </div>
   )
 }
